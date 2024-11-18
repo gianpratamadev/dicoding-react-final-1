6e95179294b3654076f734f9cc2c27c403e12b53
Gian Pratama <pratama.zidan@gmail.com>
2024-11-18 15:16:59 +0700

    initial commit

---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TextInput from "./textinput";
+
+function Header() {
+  return (
+    <header>
+      <h1>Notes</h1>
+      <TextInput />
+      <hr />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/textinput.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TextInput() {
+  return (
+    <div>
+      <input type="text" placeholder="Cari catatan..." />
+    </div>
+  );
+}
+
+export default TextInput;
